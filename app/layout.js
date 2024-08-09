c613648d50855c8ef262fc9b19797a210b916aba
Anupa <anupa@gmail.com>
2024-08-09 14:25:31 +0530

    chore: Update description in layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ import State from "/context/context";
 
 export const metadata = {
   title: "Anupa Dinuranga",
-  description: "Web portfolio of Anupa Dinuranga",
+  description: "Web portfolio of Anupa Dinuranga, a web developer and cyber security enthusiast. Anupa is a fullstack developer with a passion for web security and ethical hacking. He is a web security researcher, consultant, expert, specialist, professional, engineer, analyst, architect, auditor, trainer, instructor, coach, mentor, and tutor. Anupa is also a CTF player, competitor, enthusiast, and participant. He is skilled in networking, React, Next.js, MERN stack, and web design.",
   keywords: "Anupa Dinuranga, Web Developer, Portfolio, React, Next.js, Cyber Security, Ethical Hacking, Penetration Testing, Web Design, Web Security, Web Security Researcher, Web Security Consultant, Web Security Expert, Web Security Specialist, Web Security Professional, Web Security Engineer, Web Security Analyst, Web Security Architect, Web Security Auditor, Web Security Trainer, Web Security Instructor, Web Security Coach, Web Security Mentor, Web Security Tutor, CTF Player, CTF Competitor, CTF Enthusiast, CTF Participant, Networking, Fullstack Developer, Frontend Developer, Backend Developer, MERN Stack Developer",
   image: "https://anupadinuranga.com/images/og-image.jpg",
   url: "https://anupadinuranga.live",  
